88a1d0eb285b1b2e41e2dd6eef7c79492d3d2299
Erli Pan <git@erlipan.dev>
2021-07-26 16:36:51 +0200

    IpCam class fix bug

--- IPCam.py
+++ IPCam.py
@@ -5,13 +5,16 @@ class IPCam:
     debug = True
 
     def __init__(self, Notifier, Camera, name):
+
         self.recording = False
         self.counter = 0
         self.name = name
         self.Notifier = Notifier
         self.Camera = Camera
 
+        self.__printLog("Create updateTimer")
         self.updateTimer = threading.Timer(3.0, self.update)
+        self.__printLog("Create sendVideoTimer")
         self.sendVideoTimer  = threading.Timer(5.0, self.update)
 
 
@@ -19,41 +22,41 @@ class IPCam:
     def sendImage(self, msg):
 
         res = self.Camera.getImage()
-
         if res:
+            self.__printLog("Send photo")
             self.Notifier.sendPhoto(r)
         else:
+            self.__printLog("Send photo failed camera offine")
             self.Notifier.sendMessage("Camera offline")
-        
         return res
 
 
     def __movementTriggered(self):
-        __printLog("Movement triggered")
+        self.__printLog("Movement triggered")
         self.Notifier.sendMessage("Movimento")
         self.sendImage("Camera offine")
 
 
     def update(self):
-        __printLog("Update")
+        self.__printLog("Update")
         if self.Camera.isRecording():
-            __printLog("Camera is recording size {self.Camera.getTmpVideoSize()}")
+            self.__printLog("Camera is recording size {self.Camera.getTmpVideoSize()}")
             if recording == False:
-                __printLog("Trigger video recording")
+                self.__printLog("Trigger video recording")
                 __movementTriggered()
                 recording = True
         else:
-            __printLog("Recording ended")
+            self.__printLog("Recording ended")
             recording = False
     
     
     def sendVideo(self):
-        __printLog("Check old video")
+        self.__printLog("Check old video")
         self.Camera.callbackVideoList(self.Notifier.sendVideo)
 
 
     def __printLog(self, msg):
-        if debug:
-            print(f"{self.name} {self.counter} msg")
+        if self.debug:
+            print(f"{self.name} {self.counter} {msg}")
 
 
--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import time
 telegram = Telegram(config)
 yiCam = YiCam(config, "10.10.10.57")
 
-cam = IPCam(telegram, yiCam, "Camera 1")
-
-
 
 
 def getImmagine(update: Update, context: CallbackContext):
@@ -30,37 +27,10 @@ dispatcher.add_handler(CommandHandler("immagine", getImmagine))
 botUpdater.start_polling()
 
 
+
+cam = IPCam(telegram, yiCam, "Camera 1")
+
 print("start loop")
-rec = False
-counter = 0
 while True:
 
-    if (counter % 7 == 0):
-        print("Check old video")
-        cam.callbackVideoList(bot.sendVideo)
-
-    counter = (counter + 1) % 1000
-
-    if cam.isRecording():
-        
-        if rec == False:
-            print("Movimento")
-            bot.sendMessage("Movimento")
-            r = cam.getImage()
-
-            if r == False:
-                bot.sendMessage("Camera offline")
-            else:
-                bot.sendPhoto(r)
-
-            rec = True
-        else:
-            time.sleep(2)
-            print(f"{counter} wait tmpsize: {cam.getTmpVideoSize()}")
-    else:
-        if rec:
-            cam.callbackVideoList(bot.sendVideo)
-        else:
-            time.sleep(2)
-            print(f"{counter} sleep")
-        rec = False
+    time.sleep(2)
