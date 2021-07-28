d6ba64ef6dee5e5c144035226be4b51c0dfafe04
Erli Pan <git@erlipan.dev>
2021-07-28 02:31:39 +0200

    Fix reply markup

--- TelegramNotifyer.py
+++ TelegramNotifyer.py
@@ -10,14 +10,14 @@ class Telegram:
 
 
     def sendPhoto(self, media, caption="", reply_markup = None):
-        self.bot.sendPhoto(self.config.CHATID, media, caption=caption, parse_mode="HTML", reply_markup = reply_markup)
+        self.bot.sendPhoto(self.config.CHATID, media, caption=caption, parse_mode="HTML", reply_markup = telegram.ReplyKeyboardMarkup(reply_markup))
 
 
     def sendVideo(self, media, caption="", reply_markup = None):
-        self.bot.sendVideo(self.config.CHATID, media, caption=caption, parse_mode="HTML", reply_markup = reply_markup)
+        self.bot.sendVideo(self.config.CHATID, media, caption=caption, parse_mode="HTML", reply_markup = telegram.ReplyKeyboardMarkup(reply_markup))
 
 
     def sendMessage(self, title, message="", reply_markup = None):
         text = f"<strong>{title}</strong>\n{message}"
 
-        self.bot.sendMessage(self.config.CHATID, text, parse_mode="HTML", reply_markup = reply_markup)
+        self.bot.sendMessage(self.config.CHATID, text, parse_mode="HTML", reply_markup = telegram.ReplyKeyboardMarkup(reply_markup))
