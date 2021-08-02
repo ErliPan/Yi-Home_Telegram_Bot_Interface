e79a15a9fdc0e399d70303e9a6db286922b5045d
Erli Pan <git@erlipan.dev>
2021-08-02 02:56:44 +0200

    update msg

--- config/locale_IT.py
+++ config/locale_IT.py
@@ -47,7 +47,7 @@ def STATUS_DISABLED(CAMNAME):
 
 
 def STATUS_ONLINE(CAMNAME, NOTIFY):
-    return f"✅<code>{CAMNAME}</code> è <code>online</code>, notifiche <code>{NOTIFY}</code>\n"
+    return f"✅<code>{CAMNAME}</code> è <code>online</code>, {NOTIFY}\n"
 
 
 #Play sound function
