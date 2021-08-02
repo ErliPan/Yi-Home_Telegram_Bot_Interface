2e29a3cc8215ec8825191817ff5c13946a315f73
Erli Pan <git@erlipan.dev>
2021-08-02 14:38:18 +0200

    update locale

--- config/locale_EN.py
+++ config/locale_EN.py
@@ -1,73 +1,73 @@
-CAMERA_OFFLINE = "Camera offline"
-CAMERA_DISABLED = "Camera disabled"
-MOTION_DETECTED = "Movement detected"
-CAMERA_STATUS = "Cameras status"
+CAMERA_OFFLINE = "❌Camera offline"
+CAMERA_DISABLED = "⛔️Camera disabled"
+MOTION_DETECTED = "🏃Movement detected"
+CAMERA_STATUS = "Cameras status📹"
 
 #Say tts function
 SPEAK_LANG = "en-US"
-EMPTY_ARGS = "Error incorrect args"
+EMPTY_ARGS = "❌Error incorrect args"
 
 #Play sound function
-PLAY_VOICE = "Playing this"
+PLAY_VOICE = "📢Playing this"
 
-NOTIFICATION_YES = "Yes"
-NOTIFICATION_NO = "No"
+NOTIFICATION_YES = "🔔"
+NOTIFICATION_NO = "🔕"
 
-WAIT = "Elaborating..."
+WAIT = "⏳Elaborating...⌛️"
 
 
 def NOTIFY_ON(CAMNAME):
-    return f"Activate alert {CAMNAME}"
+    return f"🔔Activate alert {CAMNAME}"
 
 
 def NOTIFY_OFF(CAMNAME):
-    return f"Disable alert {CAMNAME}"
+    return f"🔕Disable alert {CAMNAME}"
 
 
 def TURNING_ON(CAMNAME):
-    return f"Activate on {CAMNAME}"
+    return f"🎬Activate on {CAMNAME}"
 
 
 def TURNING_OFF(CAMNAME):
-    return f"Disable {CAMNAME}"
+    return f"💤Disable {CAMNAME}"
 
 
 def IMAGE(CAMNAME):
-    return f"{CAMNAME} Picture"
+    return f"📷{CAMNAME} Picture"
 
 
 def STATUS_OFFLINE(CAMNAME):
-    return f"<code>{CAMNAME}</code> is <code>offline</code>\n"
+    return f"⛔️<code>{CAMNAME}</code> is <code>offline</code>\n"
 
 
 def STATUS_DISABLED(CAMNAME):
-    return f"<code>{CAMNAME}</code> is <code>disabilitato</code>\n"
+    return f"⚠️<code>{CAMNAME}</code> is <code>disabled</code>\n"
 
 
 def STATUS_ONLINE(CAMNAME, NOTIFY):
-    return f"<code>{CAMNAME}</code> is <code>online</code>, alerts <code>{NOTIFY}</code>\n"
+    return f"✅<code>{CAMNAME}</code> is <code>online</code>, alerts {NOTIFY}\n"
 
 
 #Play sound function
 def PLAYING_FILE(FILENAME):
-    return f"Playing file: <code>{FILENAME}</code>"
+    return f"🎶Playing file: <code>{FILENAME}</code>"
 
 
 def FILE_NOT_FOUND(FILENAME):
-    return f"File not found: <code>{FILENAME}</code>"
+    return f"❌File not found: <code>{FILENAME}</code>"
 
 
 def TTS_SAYING(TEXT):
-    return f"Saying <b><i>{TEXT}</i></b>"
+    return f"🔊Saying <b><i>{TEXT}</i></b>"
 
 
 def SET_STATUS_ON(CAMNAME):
-    return f"Activating <code>{CAMNAME}</code>..."
+    return f"⚙️Activating <code>{CAMNAME}</code>..."
 
 
 def SET_STATUS_OFF(CAMNAME):
-    return f"Deactivating <code>{CAMNAME}</code>..."
+    return f"⚙️Deactivating <code>{CAMNAME}</code>..."
 
 
 def SET_STATUS_FAILED():
-    return f"Failed operation"
+    return f"❌Failed operation"
